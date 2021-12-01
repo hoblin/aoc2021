054c1f179d4ec715b30cbe5f332038ede91232a3
Yevhenii Hurin <evgeny.gurin@gmail.com>
2021-12-01 09:54:27 +0200

    Range looks better

--- 01/02_compressed_sonar.rb
+++ 01/02_compressed_sonar.rb
@@ -6,7 +6,7 @@ measurements = File.read('01/measurements.txt').split.map(&:to_i)
 dives_count = 0
 memo = nil
 measurements.each_with_index do |_num, index|
-  sum = measurements[index] + measurements[index + 1].to_i + measurements[index + 2].to_i
+  sum = measurements[index..index + 2].sum
   dives_count += 1 if memo && memo < sum
   memo = sum
 end
