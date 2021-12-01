d456e75552532a44e05dba65d75814df38a1639d
Yevhenii Hurin <evgeny.gurin@gmail.com>
2021-12-01 09:57:28 +0200

    Times looks better

--- 01/02_compressed_sonar.rb
+++ 01/02_compressed_sonar.rb
@@ -5,7 +5,7 @@ require 'awesome_print'
 measurements = File.read('01/measurements.txt').split.map(&:to_i)
 dives_count = 0
 memo = nil
-measurements.each_with_index do |_num, index|
+measurements.length.times do |index|
   sum = measurements[index..index + 2].sum
   dives_count += 1 if memo && memo < sum
   memo = sum
