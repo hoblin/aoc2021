d8dc4eb8f6e67ee8a1038b86540aa54fa52c5656
Yevhenii Hurin <evgeny.gurin@gmail.com>
2021-12-06 19:19:21 +0200

    Day 06/01

---
+++ 06/01_tribe.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'ruby-progressbar'
+
+Fish = Struct.new(:spawn_delay) do
+  def tick
+    self.spawn_delay -= 1
+    if spawn_delay < 0
+      $tribe << Fish.new(9)
+      self.spawn_delay = 6
+    end
+  end
+end
+
+$tribe = File.read('06/input.txt').chomp.split(',').map(&:to_i).map { |delay| Fish.new(delay) }
+
+progressbar = ProgressBar.create(total: 80)
+80.times do
+  $tribe.map(&:tick)
+  progressbar.increment
+end
+p 'Result:', $tribe.size
