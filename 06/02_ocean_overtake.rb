087810aec95c144879b6d8a6c5ff5aab76fc7135
Yevhenii Hurin <evgeny.gurin@gmail.com>
2021-12-06 21:42:19 +0200

    Better naming

--- 06/02_ocean_overtake.rb
+++ 06/02_ocean_overtake.rb
@@ -4,27 +4,27 @@ require 'ruby-progressbar'
 
 LIFETIME = 256
 
-Tribe = Struct.new(:tribe) do
+Tribe = Struct.new(:school) do
   def initialize(tribe_array)
     # Instead of tracking each fish individually,
     # we can store the amount by delay left.
-    self.tribe = []
-    8.times { |i| tribe[i] = 0 }
-    tribe_array.map { |i| tribe[i] += 1 }
+    self.school = []
+    8.times { |i| school[i] = 0 }
+    tribe_array.map { |i| school[i] += 1 }
   end
 
   def tick
-    updated_tribe = []
+    updated_school = []
     8.times do |index|
-      updated_tribe[index] = tribe[index + 1].to_i
+      updated_school[index] = school[index + 1].to_i
     end
-    updated_tribe[6] += tribe[0]
-    updated_tribe[8] = tribe[0] + (updated_tribe[8] || 0)
-    self.tribe = updated_tribe
+    updated_school[6] += school[0]
+    updated_school[8] = school[0] + (updated_school[8] || 0)
+    self.school = updated_school
   end
 end
 
 tribe = Tribe.new File.read('06/input.txt').chomp.split(',').map(&:to_i)
 
 LIFETIME.times { tribe.tick }
-p 'Result:', tribe.tribe.sum
+p 'Result:', tribe.school.sum
