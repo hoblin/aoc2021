0b217489b2c6612b4e06fc602d8dbaa152146fef
Yevhenii Hurin <evgeny.gurin@gmail.com>
2021-12-06 21:38:43 +0200

    Day 06/02 solved

--- 06/02_ocean_overtake.rb
+++ 06/02_ocean_overtake.rb
@@ -1,21 +1,30 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'ruby-progressbar'
+
 LIFETIME = 256
 
-$tribe = File.read('06/input.txt').chomp.split(',').map(&:to_i)
-
-progressbar = ProgressBar.create(total: LIFETIME)
-LIFETIME.times do
-  $tribe.map! do |i|
-    i -= 1
-    if i < 0
-      $tribe << 9
-      6
-    else
-      i
-    end
+Tribe = Struct.new(:tribe) do
+  def initialize(tribe_array)
+    # Instead of tracking each fish individually,
+    # we can store the amount by delay left.
+    self.tribe = []
+    8.times { |i| tribe[i] = 0 }
+    tribe_array.map { |i| tribe[i] += 1 }
   end
-  progressbar.increment
+
+  def tick
+    updated_tribe = []
+    8.times do |index|
+      updated_tribe[index] = tribe[index + 1].to_i
+    end
+    updated_tribe[6] += tribe[0]
+    updated_tribe[8] = tribe[0] + (updated_tribe[8] || 0)
+    self.tribe = updated_tribe
+  end
 end
-p 'Result:', $tribe.size
+
+tribe = Tribe.new File.read('06/input.txt').chomp.split(',').map(&:to_i)
+
+LIFETIME.times { tribe.tick }
+p 'Result:', tribe.tribe.sum
